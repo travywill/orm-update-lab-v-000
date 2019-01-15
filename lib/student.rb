9ccbc60b5f0105c6afb085848929abc66f84b035
Travis Williamson <travywill@gmail.com>
2019-01-15 19:14:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,4 +26,12 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students;
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
 end
