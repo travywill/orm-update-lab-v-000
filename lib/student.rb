3fbf8e7cd063c3f452bc2f0f1568694b008a2af2
Travis Williamson <travywill@gmail.com>
2019-01-15 19:38:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,9 +52,6 @@ class Student
 
   def self.new_from_db(row)
     new_student = self.new(row[1], row[2], row[0])
-    #new_student.id = row[0]
-    #new_student.name = row[1]
-    #new_student.grade = row[2]
     new_student
   end
 
